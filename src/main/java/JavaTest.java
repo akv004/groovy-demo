15e1447ef2221802a8357757a37f2788946f109f
akv004 <akv004@gmail.com>
2020-10-31 18:58:52 -0500

    sorgin map

--- src/main/java/JavaTest.java
+++ src/main/java/JavaTest.java
@@ -1,10 +1,17 @@
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class JavaTest {
 
     public static void main(String args[]){
-        BigDecimal a = new BigDecimal("11.99");
-        BigDecimal b = new BigDecimal("12");
+        List<String> list = Stream.of("abc","efg").collect(Collectors.toList());
+        System.out.println(list);
+        IntStream.range(1,10).skip(5).forEach(System.out::print);
+
+
 
     }
 }
---
+++ src/main/java/sorting/SortingMapDemo.java
@@ -0,0 +1,23 @@
+package sorting;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class SortingMapDemo {
+    public static void main(String args[]){
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("a",1);
+        map.put("c",2);
+        map.put("b",3);
+        map.put("e",4);
+
+        System.out.println(map);
+
+        //Sorting map by key
+        Map newMap = map.entrySet().stream().sorted(Map.Entry.comparingByKey()).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1,e2)-> e1, LinkedHashMap::new));
+        newMap.forEach((k,v) -> System.out.println(k+", " +v ));
+    }
+}
